a52bfc6eaa172f83374eea72e52e37871fb32e40
Relissa <troshina0120@gmail.com>
2024-05-27 21:10:10 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,8 @@
+name = 'Jane'
+print( 'Name: ' + name)
+age = 29
+print('Age: ' + str(age))
+new_age = age + 1
+print( 'New age: ' + str(new_age))
+is_student = True
+print('Is student: ' + str(is_student))
